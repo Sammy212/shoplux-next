add9be4b056dffd966dfb52a9f465ba35eb6fe52
Samuel Afolabi <samuelafo.212@gmail.com>
2024-12-01 17:59:26 +0100

    Added Footer Component

---
+++ app/components/storefront/Footer.tsx
@@ -0,0 +1,25 @@
+// import Link from "next/link";
+import { Link } from "lucide-react";
+
+export function Footer() {
+    return (
+        <footer className="mt-16 mb-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex justify-between items-center border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24">
+                <p className="text-xs leading-5 text-gray-700">&copy; shopLux 2024</p>
+                <p className="text-sm leading-5 text-gray-700 flex gap-2">
+                     Created by 
+                    <a href="https://samuel-afolabi.netlify.app/" 
+                        target="_blank" 
+                        className="flex justify-center items-center gap-1 text-sm text-black font-bold hover:text-gray-400"
+                    >
+                        &nbsp;Samuel Afolabi
+                        <Link 
+                            className="w-3 h-3"
+                        /> 
+                    </a>
+                </p>
+            </div>
+
+        </footer>
+    )
+}
